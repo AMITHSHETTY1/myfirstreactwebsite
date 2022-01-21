4228354f5b3fb54b1488a5a76391c9bc0ad3a0d1
AMITH SHETTY <amith.shetty@compassitesinc.com>
2022-01-21 19:16:58 +0530

    aa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
+import Covid from './Covid.js';
 import{BrowserRouter} from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <BrowserRouter>
-<App /></BrowserRouter>,document.getElementById('root')
+<App />
+<Covid /></BrowserRouter>,document.getElementById('root')
 
 );
 
